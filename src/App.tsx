3a954957c851c3e2a90ceba3092aa23131ef2823
xStranged1 <fede.valle04@gmail.com>
2024-04-24 18:52:47 -0300

    feat: :lipstick: 10 rounds, button restart
    styles witch tailwind and shadcdn

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios, { AxiosResponse } from 'axios';
-import { Character } from './types';
-import { ImgCharacterProps, Difficult } from './types';
+import { ImgCharacterProps, Difficult, Character, ObjRequest } from './types';
 import { Skeleton } from "@/components/ui/skeleton"
 function App() {
 
+  const [firstGame, setFirstGame] = useState(true);
   const [startGame, setStartGame] = useState(false);
   const [restart, setRestart] = useState(false);
-  const [options, setOptions] = useState()
   
-  const [win, setWin] = useState(false);
+  const [finish, setFinish] = useState(false);
   const [lives, setLives] = useState(3);
+  const [countSuccess, setCountSuccess] = useState(0);
   const [indexActualCharacter, setIndexActualCharacter] = useState(0);
   const [characters, setCharacters] = useState<Character[]>();
   const [actualCharacter, setActualCharacter] = useState<Character>();
   const [difficult, setDifficult] = useState<Difficult>('medium');
+  const [skeleton, setSkeleton] = useState<number[]>();
+
   const canUseApi = true;
   const countCharacters = 826;
-  const countNeededCharacters = 3;
+  const countNeededCharacters = 10;
   const countOptions = 4;
   const URL_CHARACTERS = "https://rickandmortyapi.com/api/character/";
 
@@ -43,7 +45,11 @@ function App() {
     }
       return () => {
         console.log('call');
-        
+        let skeleton = []
+        for (let i = 0; i < countOptions; i++) {
+          skeleton.push(i)
+        }
+        setSkeleton(skeleton)
         const randonNumbers = getRandomNumbers(countCharacters, countNeededCharacters);
         if (canUseApi) getRandomsCharacters(randonNumbers)
         
@@ -51,17 +57,7 @@ function App() {
   }, [restart])
 
 
-  const handleGuess = () => {
-    if(indexActualCharacter+1 == countNeededCharacters){
-      setWin(true)
-      setStartGame(false)
-      setIndexActualCharacter(indexActualCharacter+1)
-      return
-    }
-    setActualCharacter(characters ? characters[indexActualCharacter+1] : undefined)
-    setIndexActualCharacter(indexActualCharacter+1)
-  }
-    
+  
   const getRandomNumbers = (nMax: number, countNeeded: number): number[] => { //since 0 to nMax
 
     const setOfNumbers: Set<number> = new Set();
@@ -74,25 +70,57 @@ function App() {
     return listOfNumbers;
   }
 
-  
-  const handleStart = () => {
-    if(!startGame){
-      setStartGame(true)
+  const handleSuccess = () => {
+
+    setCountSuccess(countSuccess+1)
+    
+    if(indexActualCharacter+1 == countNeededCharacters){
+      setFinish(true)
+      setStartGame(false)
+      setIndexActualCharacter(indexActualCharacter+1)
       return
     }
+    setActualCharacter(characters ? characters[indexActualCharacter+1] : undefined)
+    setIndexActualCharacter(indexActualCharacter+1)
+  }
+    
+  const handleFail = () => {
 
-    //restart
+    if(indexActualCharacter+1 == countNeededCharacters){
+      setFinish(true)
+      setStartGame(false)
+      setIndexActualCharacter(indexActualCharacter+1)
+      return
+    }
+    setActualCharacter(characters ? characters[indexActualCharacter+1] : undefined)
+    setIndexActualCharacter(indexActualCharacter+1)
+    
+  }
+  
+  const setAllStatesDefault = () => {
     restart ? setRestart(false) : setRestart(true)
+    setCountSuccess(0)
+    setFirstGame(false)
+    setFinish(false);
     setIndexActualCharacter(0)
     setCharacters(undefined)
     setActualCharacter(undefined)
+    setStartGame(true)
+  }
+  
+  const handleStart = () => {
+    if(!startGame){
+      setAllStatesDefault()
+      return
+    }
+    
   }
 
   const handleTryGuess = (guessTry: string) => {
     if(guessTry == actualCharacter?.name){
-      handleGuess();
+      handleSuccess();
     }else{
-      console.log('no adivino');
+      handleFail();
     }
   }
 
@@ -102,19 +130,24 @@ function App() {
     const [loadingImg, setLoadingImg] = useState(true);
 
     return(
-      <div className='my-8'>
-        {loadingImg && <Skeleton className="w-[250px] h-[250px] rounded-xl" /> }
-        <img className={loadingImg ? 'hidden' : 'rounded-xl b'} onLoad={() => setLoadingImg(false)} src={url} />
+      <div className='my-4'>
+        {loadingImg && <Skeleton className="w-[300px] h-[300px] p-2 rounded-xl" /> }
+        <img className={loadingImg ? 'hidden' : 'rounded-xl border-2 border-[#86c437]'} onLoad={() => setLoadingImg(false)} src={url} />
       </div>
     )
   }
 
-  const BtnGuess = ( {name} ) => {
+  const BtnGuess: React.FC<{name: string}> = ({name}) => {
 
-
+    let wonStyle = ''
+    if (name == actualCharacter?.name){
+      wonStyle = 'rounded-lg'
+    }
     return(
-      <div className='flex items-center justify-center'>
-        <button className='rounded-lg h-[30px] text-white font-semibold flex p-2 px-4 items-center justify-center h-screen bg-slate-800 '>
+      <div className='flex items-center justify-center mb-2'>
+        <button className={`${wonStyle ? wonStyle : ''} min-h-9 w-72 rounded-lg text-black text-base font-semibold flex p-2 px-4 items-center justify-center bg-[#b3edf9]`}
+          onClick={() => handleTryGuess(name)}
+        >
           {name}
         </button>
       </div>
@@ -122,61 +155,115 @@ function App() {
     )
   }
 
-  const getOptions = async () => {
+  
+  const getPage = (info: { pages: number; prev: null; next: null; }) => {
+    if (info.pages == 1) return 1
+    if (info.prev == null) return 1
+    if (info.next == null) return info.pages
 
-
-    let url = URL_CHARACTERS
-    console.log("actualCharacter?.gender");
-    console.log(actualCharacter?.gender);
-    
-    const urlGender = url+'?gender='+actualCharacter?.gender
-    const res = await axios.get(urlGender)
-    const data = res.data;
-    const pages = data.info.pages
-    const count = data.info.count
-    const rndNumbers = getRandomNumbers(count, countOptions-1)
-    
-
-    let characters: Character[] = data.results
-    
-    let requests = []
-    let pagesToGet: number[] = []
-    let indexs: number[] = [];
-    for (let i = 0; i < rndNumbers.length; i++) {
-      const num = rndNumbers[i];
-      const page = Math.floor(num/20)
-      const index = num%20;
-      indexs.push(index)
-      console.log('page');
-      console.log(page);
-      console.log("index");
-      console.log(index);
-      if (pagesToGet.includes(page)) continue
-      pagesToGet.push(page)
-      url = urlGender+'&page='+page
-      requests.push(axios.get(url))
-    }
-    const response = await axios.all(requests)
-    const options = response.map((res, i) => res.data.results[indexs[i]].name)
-    return options
+    const next = new URL(info.next);
+    const pageValue = next.searchParams.get('page')
+    if (pageValue) return parseInt(pageValue)-1
   }
 
 
+  const GuessOptions = () => {
 
-  const GuessOptions = async () => {
+    const [options, setOptions] = useState<string[]>()
+    
 
-      if (difficult == 'medium'){
-        const options = await getOptions()
-        console.log("options");
-        console.log(options);
+    useEffect(() => {
+
+      const getOptions = async () => {
+
+        let url = URL_CHARACTERS
         
+        const urlGender = url+'?gender='+actualCharacter?.gender
+        const res = await axios.get(urlGender)
+        const data = res.data;
+        const count = data.info.count
+        const rndNumbers = getRandomNumbers(count, countOptions-1)
+        
+        let requests: ObjRequest[] = []
+        let pagesToGet: number[] = []
+        for (let i = 0; i < rndNumbers.length; i++) {
+          const num = rndNumbers[i];
+          const page = Math.floor(num/20)+1
+          const index = num%20;
+
+          if (pagesToGet.includes(page)){
+            for (let j = 0; j < requests.length; j++) {
+              let req = requests[j];
+              if (req.page == page){
+                req.index.push(index)
+                break
+              }
+            }
+            continue
+          }
+
+          pagesToGet.push(page)
+          url = urlGender+'&page='+page
+          let objRequest: ObjRequest = {
+            request: axios.get(url),
+            page: page,
+            index: [index]
+          }
+          requests.push(objRequest)
+        }
+        const allRequest = requests.map((req) => req.request)
+        const responses = await axios.all(allRequest)
+        
+        let options: string[] = []
+        let totalIndex: number = 0
+        for (let i = 0; i < requests.length; i++) {
+          const req = requests[i]
+          const { index, page } = req
+          totalIndex = totalIndex + index.length
+          for (let j = 0; j < responses.length; j++) {
+            const res = responses[j];
+            const info = res.data.info
+            const actualPage = getPage(info)
+            
+            if (actualPage == page){
+              index.forEach(i => {
+                let newOption = res.data.results[i].name
+                if (newOption == actualCharacter?.name){
+                  
+                  newOption = res.data.results[i+1].name
+                  if (newOption){
+                    options.push(newOption)
+                  }else{
+                    options.push(res.data.results[i-1].name)
+                  }
+                }else{
+                  options.push(res.data.results[i].name)
+                }
+                
+              });
+              break
+            }
+          }
+        }
+        
+        const rndNum = Math.floor(Math.random()*3)
+        let auxOption = options[rndNum]
+        if (actualCharacter) options[rndNum] = actualCharacter.name
+        options.push(auxOption)
+        setOptions(options)
+        options = []
       }
+      
+      getOptions()
+
+    }, [])
+
     
     
     return(
       <div>
-          {(!options) && <p>cargando...</p>}
-          {(options) && (options.map((option) => <BtnGuess name={option} />))}
+          {(!options) && (skeleton?.map((_, i) => (<Skeleton className='h-9 w-72 mb-2 rounded-lg' key={i} />)))}
+          {(options) && (options.map((option) => <BtnGuess key={option} name={option} />))}
       </div>
       
     )
@@ -186,7 +273,7 @@ function App() {
 
     return(
       <div>
-        <p>Score: {indexActualCharacter}/{countNeededCharacters} </p>
+        <p className='text-white font-semibold'>Score: {countSuccess}/{countNeededCharacters} </p>
       </div>
     )
   }
@@ -196,28 +283,56 @@ function App() {
     return(
       <div className='flex flex-col items-center justify-center' >
         <ImgCharacter url={actualCharacter?.image} />
-        <h4>{actualCharacter?.name}</h4>
         <GuessOptions />
         <Score />
-        
       </div>
     )
   }
+  const getText = (average: number) => {
+    if (average == 1) return 'Am I Evil? Worst You are Smart 🤓';
+    if (average >= 0.9) return 'You are insane! What dimension did you come from? 🌌🌌🌌';
+    if (average >= 0.8) return 'WUBBA LUBBA DUB DUB! 💪' ;
+    if (average >= 0.7) return 'Good effort! You\'re getting closer like Morty navigating through interdimensional adventures! 🎉';
+    if (average >= 0.6) return 'You\'re doing okay, but there\'s room for improvement like Birdperson recovering from battles. 🐦';
+    if (average >= 0.5) return 'You\'re halfway there! Keep pushing like Squanchy in a party! 😺';
+    if (average >= 0.4) return 'You\'re making some progress, but there\'s more work to do like Mr. Meeseeks fulfilling requests. 📚';
+    if (average >= 0.3) return 'Keep going! You\'re making progress like Jerry trying to figure things out! 🚀';
+    if (average >= 0.2) return 'You\'re making an effort, but there\'s still a long way to go like Morty trying to survive adventures. 🚶‍♂️';
+    if (average >= 0.1) return 'You\'re as dumb as I am smart  - Rick ';
+    if (average == 0) return 'You\'ll get there! Keep trying like Rick never giving up! 💪';
+  }
+  
 
-  const YouWin = () => (
-    <div>
-      <h1>You win!</h1>
-    </div>
-  )
+  const Result = () => {
+    const average = countSuccess/countNeededCharacters
+    let text = getText(average)
+    
+    return(
+      <div className='flex flex-col items-center justify-center'>
+        <h1>Final score</h1>
+        <div>
+          <h2>{countSuccess}/{countNeededCharacters}</h2>
+        </div>
+        <div className='mt-6 mb-4'>
+          <p className='font-semibold'>{text}</p>
+        </div>
+      </div>
+    )
+  } 
 
   const BtnStart = () => {
+
     let textBtnStart = "Start!"
-    if (win){
+    
+    if (finish){
       textBtnStart = "Restart"
+    }
+    if(firstGame){
+      textBtnStart = "Start!"
     }
     
     if (!startGame) return(
-        <button className='button-start' onClick={handleStart}>
+        <button className='button-start text-[#213547]' onClick={handleStart}>
           {textBtnStart}
         </button>
     )
@@ -225,13 +340,15 @@ function App() {
   
   return (
     <div className='container mx-auto flex flex-col items-center justify-center h-screen '>
-      <div className='mt-5 px-5 p-5 bg-slate-300'>
-        <h2>Guess  rick and morty characters</h2>
+      {!finish &&(
+        <div className='mt-2 mb-2 px-5 p-5 bg-[#252c38] rounded'>
+        <h2 className='text-white font-semibold'>Guess Rick and Morty characters</h2>
         {!canUseApi && <p>desactivado</p>}
-        {(startGame && characters && !win) && <SectionGuess />} 
-      </div>
-      <div className='mt-8'>
-        {win && <YouWin />}
+        {(startGame && characters && !finish) && <SectionGuess />} 
+        </div>
+      )}
+      <div className='mt-4 flex flex-col items-center justify-center'>
+        {finish && <Result />}
         <BtnStart />
       </div>
       
